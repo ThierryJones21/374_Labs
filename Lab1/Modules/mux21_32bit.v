2ccab49b1f70f8aa9756d93e98dc16db5ac7e13f
Jamie Won <jamie.won.2000@gmail.com>
2021-02-26 18:04:44 -0500

    Removed unneeded comments

--- Lab1/Modules/mux21_32bit.v
+++ Lab1/Modules/mux21_32bit.v
@@ -1,6 +1,4 @@
 module mux21_32_bit(input [31: 0]a, b, input select, output reg muxOut);
-//   input wire [31:0] a, [31:0] b, select;
-//   output reg [31:0] muxOut;
      
     always @(a,b,select)
     begin
@@ -8,5 +6,6 @@ module mux21_32_bit(input [31: 0]a, b, input select, output reg muxOut);
         muxOut=b;
     else
         muxOut=a;
-    end    
+    end
+    
 endmodule
