cae9b602e2606fb5b6f6b6253484f432d0b65f9a
Jamie Won <jamie.won.2000@gmail.com>
2021-02-27 00:57:11 -0500

    Removed UTF invalid characters

--- Lab1/TestBenches/tb_DIV.v
+++ Lab1/TestBenches/tb_DIV.v
@@ -6,7 +6,7 @@ T0: begin//see if you need to de-assertthese signals
 end
 T1: begin
     Zlowout<= 1; PCin <= 1; Read <= 1; MDRin <= 1;
-    Mdatain <= 32'h4A920000; //opcode for AND R5, R2, R4
+    Mdatain <= 32'h4A920000; //opcode for AND R5, R2, R4
 end
 T2: begin
     MDRout<= 1; IRin <= 1;
