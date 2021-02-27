7515f5114308d1a62f1bbc51c094b5415e0c2a69
Naod Dereje <17nd26@queensu.ca>
2021-02-27 16:43:41 -0500

    Datapath Updated with new Register Logic

--- Lab1/datapath.v
+++ Lab1/datapath.v
@@ -1,6 +1,10 @@
-module datapath(input PCout, Zlowout, MDRout, R2out, R4out, MARin, Zin, 
-					 PCin, MDRin, IRin, Yin, IncPC, Read, CONTROL, R5in, R2in, R4in, Clock, Mdatain, Clear, R0out, R1out, R2out, R3out, R4out, R5out, R6out
-					 R7out,R8out,R9out,R10out,R11out,R12out,R13out,R14out,R15out);    
+module datapath(input PCout, Zlowout, MDRout, MARin, Zin, 
+					 PCin, MDRin, IRin, Yin, IncPC, Read, CONTROL,Clock, 
+					 Mdatain, Clear, R0out, R1out, R2out, R3out, R4out, R5out, R6out,
+					 R7out,R8out,R9out,R10out,R11out,R12out,R13out,R14out,R15out,
+					R0in, R1in, R2in, R3in, R4in, R5in, R6in,
+					 R7in, R8in, R9in,R10in,R11in,R12in,R13in,R14in,R15in
+					 );    
 
 			
 			wire [31:0] Bus_Mux_Out;
@@ -9,22 +13,22 @@ module datapath(input PCout, Zlowout, MDRout, R2out, R4out, MARin, Zin,
 			reg [31:0] ALU_HI, ALU_LO;
 			wire[63:0] C;
 					
-			Register myR0 (Clock, Clear, Bus_Mux_Out, R0out);
-         Register myR1 (Clock, Clear, Bus_Mux_Out, R1out); 
-			Register myR1 (Clock, Clear, Bus_Mux_Out, R2out); 
-			Register myR1 (Clock, Clear, Bus_Mux_Out, R3out); 
-			Register myR1 (Clock, Clear, Bus_Mux_Out, R4out); 
-			Register myR1 (Clock, Clear, Bus_Mux_Out, R5out); 
-			Register myR1 (Clock, Clear, Bus_Mux_Out, R6ut); 
-			Register myR1 (Clock, Clear, Bus_Mux_Out, R7out); 
-			Register myR1 (Clock, Clear, Bus_Mux_Out, R8out); 
-			Register myR1 (Clock, Clear, Bus_Mux_Out, R9out); 
-			Register myR1 (Clock, Clear, Bus_Mux_Out, R10out); 
-			Register myR1 (Clock, Clear, Bus_Mux_Out, R11out);  
-			Register myR1 (Clock, Clear, Bus_Mux_Out, R12out); 
-			Register myR1 (Clock, Clear, Bus_Mux_Out, R13out); 
-			Register myR1 (Clock, Clear, Bus_Mux_Out, R14out); 
-			Register myR1 (Clock, Clear, Bus_Mux_Out, R15out); 
+         Register myR0 (Clock, Clear, Bus_Mux_Out, R0in, R0out); 
+         Register myR1 (Clock, Clear, Bus_Mux_Out, R1in, R1out); 
+			Register myR2 (Clock, Clear, Bus_Mux_Out, R2in, R2out); 
+			Register myR3 (Clock, Clear, Bus_Mux_Out, R3in, R3out); 
+			Register myR4 (Clock, Clear, Bus_Mux_Out, R4in, R4out); //add regiteser output 
+			Register myR5 (Clock, Clear, Bus_Mux_Out, R5in, R5out); 
+			Register myR6 (Clock, Clear, Bus_Mux_Out, R6in, R6out); 
+			Register myR7 (Clock, Clear, Bus_Mux_Out, R7in, R7out); 
+			Register myR8 (Clock, Clear, Bus_Mux_Out, R8in, R8out); 
+			Register myR9 (Clock, Clear, Bus_Mux_Out, R9in, R9out); 
+			Register myR10 (Clock, Clear, Bus_Mux_Out, R10in, R10out); 
+			Register myR11 (Clock, Clear, Bus_Mux_Out, R11in, R11out);  
+			Register myR12 (Clock, Clear, Bus_Mux_Out, R12in, R12out); 
+			Register myR13 (Clock, Clear, Bus_Mux_Out, R13in, R13out); 
+			Register myR14 (Clock, Clear, Bus_Mux_Out, R14in, R14out); 
+			Register myR15 (Clock, Clear, Bus_Mux_Out, R15in, R15out); 
 								
 														
 			Bus myBusMux (R0_out, R1_out, R2_out, R3_out, R4_out, R5_out, R6_out, R7_out, R8_out, R9_out,  R10_out, R11_out, R12_out, R13_out, R14_out, R15_out, HI_out, LO_out, Z_high_out, Z_low_out, PC_out, MDR_out, In_Portout, C_out,
