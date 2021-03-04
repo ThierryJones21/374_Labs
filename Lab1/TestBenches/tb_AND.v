5ef38ab018477abedde3889609d632fb095d26bd
Thierry Jones <50874546+ThierryJones21@users.noreply.github.com>
2021-03-04 16:56:39 -0500

    Update tb_AND.v

--- Lab1/TestBenches/tb_AND.v
+++ Lab1/TestBenches/tb_AND.v
@@ -3,7 +3,7 @@ T0: begin//see if you need to de-assertthese signals
 end
 T1: begin
     Zlowout<= 1; PCin <= 1; Read <= 1; MDRin <= 1;
-    Mdatain <= 32'h4A920000; //opcode for AND R5, R2, R4
+    Mdatain <= 32'h4A920000; //opcode for AND R5, R2, R4 01001 0101 0010 0100 0000 0000 0000 000 pad 3 bits
 end
 T2: begin
     MDRout<= 1; IRin <= 1;
