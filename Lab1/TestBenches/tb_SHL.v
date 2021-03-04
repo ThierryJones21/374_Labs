fc8e9ec495041a62afb819faf5090cced83f61d6
Naod Dereje <50122055+naoddereje@users.noreply.github.com>
2021-03-04 17:13:38 -0500

    changed the opcode for SHL instruction

--- Lab1/TestBenches/tb_SHL.v
+++ Lab1/TestBenches/tb_SHL.v
@@ -3,7 +3,7 @@ T0: begin//see if you need to de-assertthese signals
 end
 T1: begin
     Zlowout<= 1; PCin <= 1; Read <= 1; MDRin <= 1;
-    Mdatain <= 32'h4A920000; //opcode for AND R5, R2, R4
+    Mdatain <= 32'h32920000; //opcode for SHL R5, R2, R4 --> 0011 0010 1001 0010 0000
 end
 T2: begin
     MDRout<= 1; IRin <= 1;
