547962afb438e85b32ccbb07a8ba04f37354c6b9
Thierry Jones <50874546+ThierryJones21@users.noreply.github.com>
2021-03-04 17:08:24 -0500

    Update tb_NOT.v

--- Lab1/TestBenches/tb_NOT.v
+++ Lab1/TestBenches/tb_NOT.v
@@ -3,7 +3,7 @@ T0: begin//see if you need to de-assertthese signals
 end
 T1: begin
     Zlowout<= 1; PCin <= 1; Read <= 1; MDRin <= 1;
-    Mdatain <= 32'h4A920000; //opcode for AND R5, R2, R4
+    Mdatain <= 32'h8A90000; //opcode for NOT R5, R2 10001 0101 0010 000 0000 0000 0000 
 end
 T2: begin
     MDRout<= 1; IRin <= 1;
