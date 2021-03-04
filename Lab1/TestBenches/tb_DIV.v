36495e995796af4a6ed3bf5ed854516033e0dd88
Thierry Jones <50874546+ThierryJones21@users.noreply.github.com>
2021-03-04 17:01:28 -0500

    Update tb_DIV.v

--- Lab1/TestBenches/tb_DIV.v
+++ Lab1/TestBenches/tb_DIV.v
@@ -6,7 +6,7 @@ T0: begin//see if you need to de-assertthese signals
 end
 T1: begin
     Zlowout<= 1; PCin <= 1; Read <= 1; MDRin <= 1;
-    Mdatain <= 32'h7A920000; //opcode for AND R5, R2, R4 0111 1010 1001 0010 0000 0000 0000 0000 
+    Mdatain <= 32'h7920000; //opcode for DIV R2, R4 01111 0010 0100 000 0000 0000 0000 
 end
 T2: begin
     MDRout<= 1; IRin <= 1;
