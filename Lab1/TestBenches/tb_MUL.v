9297770a7fa8ded3d01e21b1ba898a26e7c4385f
Thierry Jones <50874546+ThierryJones21@users.noreply.github.com>
2021-03-04 16:52:43 -0500

    Update tb_MUL.v

--- Lab1/TestBenches/tb_MUL.v
+++ Lab1/TestBenches/tb_MUL.v
@@ -5,7 +5,7 @@ T0: begin//see if you need to de-assertthese signals
 end
 T1: begin
     Zlowout<= 1; PCin <= 1; Read <= 1; MDRin <= 1;
-    Mdatain <= 32'h72920000; //opcode for AND R5, R2, R4 0111 0010 1001 0010 0000 0000 0000 0000 
+    Mdatain <= 32'hE240000; //opcode for MUL R2, R4 0111 0001 0010 0000 0000 0000 0000 0
 end
 T2: begin
     MDRout<= 1; IRin <= 1;
