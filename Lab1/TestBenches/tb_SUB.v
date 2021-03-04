635b3869812fb1e577ae743cd911fe64cded82cb
Thierry Jones <50874546+ThierryJones21@users.noreply.github.com>
2021-03-04 16:57:46 -0500

    Update tb_SUB.v

--- Lab1/TestBenches/tb_SUB.v
+++ Lab1/TestBenches/tb_SUB.v
@@ -3,7 +3,7 @@ T0: begin//see if you need to de-assertthese signals
 end
 T1: begin
     Zlowout<= 1; PCin <= 1; Read <= 1; MDRin <= 1;
-    Mdatain <= 32'h22920000; //opcode for AND R5, R2, R4 00100010100100100000000000000000 
+    Mdatain <= 32'h22920000; //opcode for SUB R5, R2, R4 00100 0101 0010 0100 0000 0000 0000 000 pad 3 bits
 end
 T2: begin
     MDRout<= 1; IRin <= 1;
