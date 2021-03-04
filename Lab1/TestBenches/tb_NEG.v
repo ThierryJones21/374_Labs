b7b11a4423ca6f4de2ea35ebae3a19b2e1f0b45e
Thierry Jones <50874546+ThierryJones21@users.noreply.github.com>
2021-03-04 17:06:05 -0500

    Update tb_NEG.v

--- Lab1/TestBenches/tb_NEG.v
+++ Lab1/TestBenches/tb_NEG.v
@@ -3,7 +3,7 @@ T0: begin//see if you need to de-assertthese signals
 end
 T1: begin
     Zlowout<= 1; PCin <= 1; Read <= 1; MDRin <= 1;
-    Mdatain <= 32'h82920000; //opcode for AND R5, R2, R4
+    Mdatain <= 32'h82A0000; //opcode for NEG R5, R2 10000 0101 0100 000 0000 0000 0000 
 end
 T2: begin
     MDRout<= 1; IRin <= 1;
