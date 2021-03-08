c37c8a85ed4f7f60ee18daaf87062a1bdef3d548
Jamie Won <jamie.won.2000@gmail.com>
2021-03-08 00:53:21 -0500

    Untested con_ff logic v2 in comments

--- Lab2/con_ff.v
+++ Lab2/con_ff.v
@@ -17,3 +17,24 @@ module con_ff (con_in, IR, BusMuxOut, toControlUnit);
     end else toControlUnit <= 0; // set 0
 
 endmodule
+
+// module con_ff (con_in, IR, BusMuxOut, toControlUnit);
+
+//     input con_in; // enable
+//     input [3:0] IR;
+//     input [31:0] BusMuxOut;
+//     output [31:0] toControlUnit; // this is PC + 1 + C (signExtended)
+
+//     // Note: Nor with same input is a not. Negated again, it is the original.
+//     // Note: AND ing a 32 bit number and 1 bit number means only the last bit matters?
+//     if (con_in) begin
+//         case (IR)
+//             0: toControlUnit <= !BusMuxOut[0]; // branch if zero
+//             1: toControlUnit <= BusMuxOut[0]; // branch if non zero
+//             2: toControlUnit <= !BusMuxOut[31]; // branch if greater than zero
+//             3: toControlUnit <= BusMuxOut[31]; // branch if less than zero
+//             default: begin end; // otherwise do nothing
+//         endcase
+//     end else toControlUnit <= 0; // set 0
+
+// endmodule
