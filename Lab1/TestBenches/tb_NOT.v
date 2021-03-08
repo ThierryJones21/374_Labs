f7388e885bc019efa355a02f5f5adab666e88eae
Jamie Won <jamie.won.2000@gmail.com>
2021-03-08 09:08:51 -0500

    Deasserts signals in testbench

--- Lab1/TestBenches/tb_NOT.v
+++ Lab1/TestBenches/tb_NOT.v
@@ -1,16 +1,21 @@
 T0: begin//see if you need to de-assertthese signals
     #10 PCout<= 1; MARin <= 1; IncPC <= 1; Zlowin <= 1;
+    #15 PCout<= 0; MARin <= 0; IncPC <= 0; Zlowin <= 0;
 end
 T1: begin
     #10 Zlowout<= 1; PCin <= 1; Read <= 1; MDRin <= 1; Mdatain <= 32'h8A90000; //opcode for NOT R5, R2 10001 0101 0010 000 0000 0000 0000 
+    #15 Zlowout<= 0; PCin <= 0; Read <= 0; MDRin <= 0;
 end
 T2: begin
     #10 MDRout<= 1; IRin <= 1;
+    #15 MDRout<= 0; IRin <= 0;
 end
 T3: begin
     // not
     #10 R2out<= 1; CONTROL <= 5; Zlowin <= 1;
+    #15 R2out<= 0; Zlowin <= 0;
 end
 T4: begin
     #10 Zlowout<= 1; R5in <= 1;
+    #15 Zlowout<= 0; R5in <= 0;
 end
