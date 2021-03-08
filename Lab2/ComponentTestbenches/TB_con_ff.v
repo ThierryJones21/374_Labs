8567985c4202a1de6437b939cfcacf53e9e3eb2d
Jamie Won <jamie.won.2000@gmail.com>
2021-03-08 11:18:50 -0500

    con_ff.v should work. Testbench is a bit fuzzy, but should be okay too

--- Lab2/ComponentTestbenches/TB_con_ff.v
+++ Lab2/ComponentTestbenches/TB_con_ff.v
@@ -1,11 +1,12 @@
-module TB_con_ff (reg [31:0] toControlUnit);
+module TB_con_ff ();
 
     reg con_in, Clock;
     reg [3:0] IR;
     reg [31:0] BusMuxOut;
+    reg toControlUnit;
 
     reg[3:0] Present_state= Default;
-    parameter Default = 4'b0000, T0= 4'b0001, T1= 4'b0010, T2= 4'b0011, T3=4'b0100 ;
+    parameter Default = 4'b0000, T0= 4'b0001, T1= 4'b0010, T2= 4'b0011, T3=4'b0100, T4= 4'b0101, T5= 4'b0110, T6= 4'b0111 ;
 
     con_ff my_on_ff (con_in, IR, BusMuxOut, toControlUnit);
 
@@ -21,6 +22,9 @@ module TB_con_ff (reg [31:0] toControlUnit);
             T0          :   Present_state = T1;
             T1          :   Present_state = T2;
             T2          :   Present_state = T3;
+            T3          :   Present_state = T4;
+            T4          :   Present_state = T5;
+            T5          :   Present_state = T6;
         endcase
     end
 
@@ -28,28 +32,52 @@ module TB_con_ff (reg [31:0] toControlUnit);
     begin 
         case (Present_state)
             default: begin
-                // default 7 value 
-                // should read XXX
+                // should be 1
 				con_in <= 1;
                 IR <= 0;
-                BusMuxOut <= 10011001100110011001100110011001;
+                BusMuxOut <= 0;
             end
             T0: begin 
+                // should be 0
                 IR <= 1;
-                BusMuxOut <= 10011001100110011001100110011001
+                BusMuxOut <= 0;
             end
             T1: begin 
+                // should be 1
                 IR <= 2;
-                BusMuxOut <= 10011001100110011001100110011001
+                BusMuxOut <= 10;
             end
             T2: begin 
+                // should be 0
                 IR <= 3;
-                BusMuxOut <= 10011001100110011001100110011001
+                BusMuxOut <= 10;
+            end
+            T3: begin
+                // should be 0
+				con_in <= 1;
+                IR <= 0;
+                BusMuxOut <= 7;
+            end
+            T4: begin 
+                // should be 1
+                IR <= 1;
+                BusMuxOut <= 7;
+            end
+            T5: begin 
+                // should be 0
+                IR <= 2;
+                BusMuxOut <= -10;
+            end
+            T6: begin 
+                // should be 1
+                IR <= 3;
+                BusMuxOut <= -10;
             end
         endcase
     end
 
-    always @(toControlUnit) begin
+    always @(IR) begin
+        $display("IR: %b", IR);
         $display("%b", toControlUnit);
     end
 
--- Lab2/con_ff.v
+++ Lab2/con_ff.v
@@ -3,40 +3,32 @@ module con_ff (con_in, IR, BusMuxOut, toControlUnit);
     input con_in; // enable
     input [3:0] IR;
     input [31:0] BusMuxOut;
-    output reg [31:0] toControlUnit; // this is PC + 1 + C (signExtended)
+    output reg toControlUnit; // this is PC + 1 + C (signExtended)
+
+    function triple_nor;
+        input [31:0] BusMuxOut;
+        // as long as 1 bit is 1, return true. Since this is then put through a NOT gate, it means
+        // as long as 1 bit is 1, return false
+        // that is, if zero, return true
+        triple_nor = (BusMuxOut == 0) ? 1 : 0;
+    endfunction
+
     always @(posedge con_in) begin
         
     // Note: Nor with same input is a not. Negated again, it is the original.
         if (con_in) begin
+            $display("%b", triple_nor(BusMuxOut));
+            $display("%b", !triple_nor(BusMuxOut));
+            $display("%b", !BusMuxOut[31]);
+            $display("%b", BusMuxOut[31]);
             case (IR)
-                0: toControlUnit <= !BusMuxOut; // branch if zero
-                1: toControlUnit <= BusMuxOut; // branch if non zero
-                2: toControlUnit <= !BusMuxOut[31]; // branch if greater than zero
-                3: toControlUnit <= BusMuxOut[31]; // branch if less than zero
+                0: toControlUnit = triple_nor(BusMuxOut); // branch if zero
+                1: toControlUnit = !triple_nor(BusMuxOut); // branch if non zero
+                2: toControlUnit = !BusMuxOut[31]; // branch if greater than zero
+                3: toControlUnit = BusMuxOut[31]; // branch if less than zero
                 default: begin end // otherwise do nothing
             endcase
         end else toControlUnit <= 0; // set 0
     end
 
 endmodule
-
-// module con_ff (con_in, IR, BusMuxOut, toControlUnit);
-
-//     input con_in; // enable
-//     input [3:0] IR;
-//     input [31:0] BusMuxOut;
-//     output reg [31:0] toControlUnit; // this is PC + 1 + C (signExtended)
-
-//     // Note: Nor with same input is a not. Negated again, it is the original.
-//     // Note: AND ing a 32 bit number and 1 bit number means only the last bit matters?
-//     if (con_in) begin
-//         case (IR)
-//             0: toControlUnit <= !BusMuxOut[0]; // branch if zero
-//             1: toControlUnit <= BusMuxOut[0]; // branch if non zero
-//             2: toControlUnit <= !BusMuxOut[31]; // branch if greater than zero
-//             3: toControlUnit <= BusMuxOut[31]; // branch if less than zero
-//             default: begin end // otherwise do nothing
-//         endcase
-//     end else toControlUnit <= 0; // set 0
-
-// endmodule
