60a9071ffd3ddf5907163669c15d73e207663aa0
Jamie Won <jamie.won.2000@gmail.com>
2021-03-15 09:01:30 -0400

    Fixed ALU

--- Lab2/ALU.v
+++ Lab2/ALU.v
@@ -9,7 +9,7 @@ module ALU (input[31:0] A, B, output reg[31:0] C_LO, C_HI, input wire [3:0]cntrl
 	mul booth (booth_result, A, B);
 
 	always @(*) begin
-    	case(cntrl)
+		case(cntrl)
 			11	:	begin // division
 						C_LO = div_quotient[31:0];	
 						C_HI = div_quotient[63:32];
@@ -18,17 +18,11 @@ module ALU (input[31:0] A, B, output reg[31:0] C_LO, C_HI, input wire [3:0]cntrl
 						C_LO = booth_result[31:0];
 						C_HI = booth_result [63:32];
 					end
-			9   :   begin  // rotate right
-						C_LO[31:0]= 1 >> B;
-						C_LO[31] = B[0];
-					end
-			8   :   begin // rotate left
-						C_LO[31:0]= 1 << B;
-						C_LO[0] = B[31];
-					end
+			9   :   C_LO = A >> B | A << (32 - B); // rotate right
+			8   :	C_LO = A << B | A >> (32 - B); // rotate left
 			7   :   C_LO[31:0] = A >>> B; // right arithmetic shift - A = how many shifts, B = the number you want to shift 
 			6   :   C_LO[31:0] = A <<< B; // left arithtmatic shift - A = how many shifts, B = the number you want to shift 
-			5   :   C_LO[31:0] = !B; // logical not 
+			5   :   C_LO[31:0] = ~B; // logical not 
 			4   :   C_LO[31:0] = -B; //negation function
 			3   :   C_LO[31:0] = A - B;
 			2   :   C_LO[31:0] = A + B;
