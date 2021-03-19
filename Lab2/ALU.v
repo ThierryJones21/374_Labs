a7bd80e0f7248ffd38a8dfe829f19893bee6f996
Jamie Won <jamie.won.2000@gmail.com>
2021-03-19 15:03:10 -0400

    Fixed latching issues, removed whitespace

--- Lab2/ALU.v
+++ Lab2/ALU.v
@@ -9,6 +9,8 @@ module ALU (input[31:0] A, B, output reg[31:0] C_LO, C_HI, input wire [3:0]cntrl
 	mul booth (booth_result, A, B);
 
 	always @(*) begin
+		C_LO = 0;
+		C_HI = 0;
 		case(cntrl)
 			11	:	begin // division
 						C_LO = div_quotient[31:0];	
@@ -20,17 +22,15 @@ module ALU (input[31:0] A, B, output reg[31:0] C_LO, C_HI, input wire [3:0]cntrl
 					end
 			9   :   C_LO = A >> B | A << (32 - B); // rotate right
 			8   :	C_LO = A << B | A >> (32 - B); // rotate left
-			7   :   C_LO[31:0] = A >>> B; // right arithmetic shift - A = how many shifts, B = the number you want to shift 
-			6   :   C_LO[31:0] = A <<< B; // left arithtmatic shift - A = how many shifts, B = the number you want to shift 
-			5   :   C_LO[31:0] = ~B; // logical not 
-			4   :   C_LO[31:0] = -B; //negation function
-			3   :   C_LO[31:0] = A - B;
-			2   :   C_LO[31:0] = A + B;
-			1   :   C_LO[31:0] = A | B;
-			0   :   C_LO[31:0] = A & B;
+			7   :   C_LO = A >>> B; // right arithmetic shift - A = how many shifts, B = the number you want to shift 
+			6   :   C_LO = A <<< B; // left arithtmatic shift - A = how many shifts, B = the number you want to shift 
+			5   :   C_LO = ~B; // logical not 
+			4   :   C_LO = -B; //negation function
+			3   :   C_LO = A - B;
+			2   :   C_LO = A + B;
+			1   :   C_LO = A | B;
+			0   :   C_LO = A & B;
 			default	:	begin end
 		endcase
 	end
 endmodule
-
-
