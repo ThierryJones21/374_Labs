2c2d013470b49e7d7d5ea51f73a431ee18c8e9cd
Jamie Won <jamie.won.2000@gmail.com>
2021-03-19 19:04:41 -0400

    jr instruction

--- Lab2/InstructionTestBenches/jr_TB.v
+++ Lab2/InstructionTestBenches/jr_TB.v
@@ -1,4 +1,4 @@
-module jr_TB ();//uses ConFF to determine what branch instruction to use op code is different however
+//uses ConFF to determine what branch instruction to use op code is different however
 // opcode for jr R1 (10011xxxx------------------------)//   if R15 it is for precedure return
   
 always @(Present_state)// do the required job ineach state
@@ -8,16 +8,18 @@ begin
             PCout <= 0; Zlowout <= 0; Zhighout <= 0; MDRout<= 0;  //initialize the signals
             MARin <= 0; Zlowin <= 0; Zhighin <= 0;
             PCin <= 0; MDRin <= 0; IRin  <= 0; Yin <= 0;
-            IncPC <= 0; Read <= 0; 
-            Mdatain <= 32'h00000000; Rout <= 0; 
-            Gra <= 0; 
-        end    
+            IncPC <= 0; Read <= 0; CONTROL <= 0;
+            Clear <= 1; Yout <= 0; highin <= 0; lowin <= 0;
+            Cout <= 0; outPortIn <= 0; inPortOut <= 0; inPortIn<= 0; con_in <= 0;
+            Mdatain <= 32'h00000000; highout <= 0; lowout <= 0;
+            BAout <= 0; Rin <= 0; Rout <= 0; Gra <= 0; Grb <= 0; Grc <= 0; ram_enable <= 0;
+        end   
         T0: begin 
-            #10 PCout <= 1; MARin <= 1; IncPC <= 1; Zin<= 1;  //initialize the signals         
-            #15 PCout <= 0; MARin <= 0; IncPC <= 0; Zin<= 0;
+            #10 PCout <= 1; MARin <= 1; IncPC <= 1; Zlowin<= 1;  //initialize the signals         
+            #15 PCout <= 0; MARin <= 0; IncPC <= 0; Zlowin<= 0;
         end
         T1: begin
-            #10 Zlowout<= 1; PCin <= 1; Read <= 1; MDRin <= 1; Mdatain <= 32'h9880000; // opcode for jr R1 (1001100010000000000000000) 000//  
+            #10 Zlowout<= 1; PCin <= 1; Read <= 1; MDRin <= 1; //Mdatain <= 32'h98800000; // opcode for jr R1 (1001100010000000000000000) 000//  
             #15 Zlowout<= 0; PCin <= 0; Read <= 0; MDRin <= 0;
         end
         T2: begin
@@ -30,5 +32,3 @@ begin
         end
     endcase
 end
-
-endmodule 
