29c3a42f77eff29cda2c307ff1f9bc16cecaec1c
Jamie Won <jamie.won.2000@gmail.com>
2021-03-19 00:12:05 -0400

    Cleaning up some select and encode logic, prevents constant switching

--- Lab2/select_and_encode.v
+++ Lab2/select_and_encode.v
@@ -3,7 +3,7 @@ module select_and_encode (Gra, Grb, Grc, Rin, Rout, BAout, IR, RXin, RXout, CSig
     input Gra, Grb, Grc, Rin, Rout, BAout; // from CPU
     input [31:0] IR;
     output reg [15:0] RXin, RXout; // Registers 0 - 15 in/out
-    output [31:0] CSignExtended;
+    output reg [31:0] CSignExtended;
 
     reg [3:0] selected_register;
 
@@ -15,18 +15,20 @@ module select_and_encode (Gra, Grb, Grc, Rin, Rout, BAout, IR, RXin, RXout, CSig
         if (Grb == 1) selected_register = IR[22:19]; else
         if (Grc == 1) selected_register = IR[18:15];
 		  
-		  if (BAout == 1|| Rout == 1) begin 
-				RXout = 0;
-				RXout[selected_register] = 1;
-		  end else 
+        if (BAout == 1|| Rout == 1) begin 
+            RXout = 0;
+            RXout[selected_register] = 1;
+            // C_sign_extended
+            CSignExtended = $signed(IR[18:0]);
+        end else 
         if (Rin) begin 
-				RXin = 0;
-				RXin[selected_register] = 1;
-		  end
+            RXin = 0;
+            RXin[selected_register] = 1;
+            // C_sign_extended
+            CSignExtended = $signed(IR[18:0]);
+        end
 
     end
-    // C_sign_extended
-    assign CSignExtended = $signed(IR[18:0]);
 
 
 endmodule
