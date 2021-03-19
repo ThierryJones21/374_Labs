90f67ab22066be2a66c9d3e17e1fb92313e64758
Jamie Won <jamie.won.2000@gmail.com>
2021-03-19 15:03:56 -0400

    Fixed latching issues in select_and_encode.v

--- Lab2/select_and_encode.v
+++ Lab2/select_and_encode.v
@@ -3,12 +3,15 @@ module select_and_encode (Gra, Grb, Grc, Rin, Rout, BAout, IR, RXin, RXout, CSig
     input Gra, Grb, Grc, Rin, Rout, BAout; // from CPU
     input [31:0] IR;
     output reg [15:0] RXin, RXout; // Registers 0 - 15 in/out
-    output reg [31:0] CSignExtended;
+    output [31:0] CSignExtended;
 
     reg [3:0] selected_register;
 
     always @(Gra, Grb, Grc, IR, BAout, Rin, Rout) // are these the right thingsin the sensitivity list
     begin
+        RXout = 0;
+        RXin = 0;
+        selected_register = 0;
         
         // Select
         if (Gra == 1) selected_register = IR[26:23]; else
@@ -16,19 +19,13 @@ module select_and_encode (Gra, Grb, Grc, Rin, Rout, BAout, IR, RXin, RXout, CSig
         if (Grc == 1) selected_register = IR[18:15];
 		  
         if (BAout == 1|| Rout == 1) begin 
-            RXout = 0;
             RXout[selected_register] = 1;
-            // C_sign_extended
-            CSignExtended = $signed(IR[18:0]);
         end else 
         if (Rin) begin 
-            RXin = 0;
             RXin[selected_register] = 1;
-            // C_sign_extended
-            CSignExtended = $signed(IR[18:0]);
         end
 
     end
 
-
-endmodule
+    assign CSignExtended = $signed(IR[18:0]);
+endmodule 
