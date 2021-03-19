006aebad5b0215d956882df966fa968a36a3a145
Thierry Jones <50874546+ThierryJones21@users.noreply.github.com>
2021-03-19 15:24:40 -0400

    Update brpl_TB.v

--- Lab2/InstructionTestBenches/brpl_TB.v
+++ Lab2/InstructionTestBenches/brpl_TB.v
@@ -17,7 +17,7 @@ begin
             #15 PCout <= 0; MARin <= 0; IncPC <= 0; Zin<= 0;
         end
         T1: begin
-            #10 Zlowout<= 1; PCin <= 1; Read <= 1; MDRin <= 1; Mdatain <= 32'h9111818; // opcode for brpl PC <- PC + 1 + C (1001000100010001100000011) 000//  
+            #10 Zlowout<= 1; PCin <= 1; Read <= 1; MDRin <= 1; Mdatain <= 32'h9111818; // opcode for brpl R2, 35 (1001000100010001100000011) 000//  
             #15 Zlowout<= 0; PCin <= 0; Read <= 0; MDRin <= 0;
         end
         T2: begin
