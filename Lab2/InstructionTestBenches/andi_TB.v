939916c96fd7b392e20073971a1b0a2cca2f3dc5
Jamie Won <jamie.won.2000@gmail.com>
2021-03-20 13:26:55 -0400

    control sequences with complete default

--- Lab2/InstructionTestBenches/andi_TB.v
+++ Lab2/InstructionTestBenches/andi_TB.v
@@ -9,7 +9,7 @@ begin
             Clear <= 1; Yout <= 0; highin <= 0; lowin <= 0;
             Cout <= 0; outPortIn <= 0; inPortOut <= 0; inPortIn<= 0; con_in <= 0;
             Mdatain <= 32'h00000000; highout <= 0; lowout <= 0;
-            BAout <= 0; Rin <= 0; Rout <= 0; Gra <= 0; Grb <= 0; Grc <= 0; ram_enable <= 0;
+            BAout <= 0; Rin <= 0; Rout <= 0; Gra <= 0; Grb <= 0; Grc <= 0; ram_enable <= 0; R15_enable <= 0;
         end    
         T0: begin 
             #10 PCout <= 1; MARin <= 1; IncPC <= 1; Zlowin <= 1; // Mdatain <= 32'h61080026; opcode for ANDI R2, R1, $26 : 01100 0010 0001 000 0000 0000 0010 0110 in init file
