41835084515f8bcca1e6a6a6d125f05375a929c1
Jamie Won <jamie.won.2000@gmail.com>
2021-03-19 20:23:53 -0400

    Adding the incpc function to the ALU

--- Lab2/ALU.v
+++ Lab2/ALU.v
@@ -1,4 +1,4 @@
-module ALU (input[31:0] A, B, output reg[31:0] C_LO, C_HI, input wire [3:0]cntrl);
+module ALU (input[31:0] A, B, output reg[31:0] C_LO, C_HI, input wire [3:0]cntrl, input IncPC);
 
 	wire[63:0] div_quotient;
 	
@@ -11,26 +11,29 @@ module ALU (input[31:0] A, B, output reg[31:0] C_LO, C_HI, input wire [3:0]cntrl
 	always @(*) begin
 		C_LO = 0;
 		C_HI = 0;
-		case(cntrl)
-			11	:	begin // division
-						C_LO = div_quotient[31:0];	
-						C_HI = div_quotient[63:32];
-					end
-			10  :   begin // multiplication
-						C_LO = booth_result[31:0];
-						C_HI = booth_result [63:32];
-					end
-			9   :   C_LO = A >> B | A << (32 - B); // rotate right
-			8   :	C_LO = A << B | A >> (32 - B); // rotate left
-			7   :   C_LO = A >>> B; // right arithmetic shift - A = how many shifts, B = the number you want to shift 
-			6   :   C_LO = A <<< B; // left arithtmatic shift - A = how many shifts, B = the number you want to shift 
-			5   :   C_LO = ~B; // logical not 
-			4   :   C_LO = -B; //negation function
-			3   :   C_LO = A - B;
-			2   :   C_LO = A + B;
-			1   :   C_LO = A | B;
-			0   :   C_LO = A & B;
-			default	:	begin end
-		endcase
+		if (IncPC) C_LO = B + 1; // increases program counter
+		else begin 
+			case(cntrl)
+				11	:	begin // division
+							C_LO = div_quotient[31:0];	
+							C_HI = div_quotient[63:32];
+						end
+				10  :   begin // multiplication
+							C_LO = booth_result[31:0];
+							C_HI = booth_result [63:32];
+						end
+				9   :   C_LO = A >> B | A << (32 - B); // rotate right
+				8   :	C_LO = A << B | A >> (32 - B); // rotate left
+				7   :   C_LO = A >>> B; // right arithmetic shift - A = how many shifts, B = the number you want to shift 
+				6   :   C_LO = A <<< B; // left arithtmatic shift - A = how many shifts, B = the number you want to shift 
+				5   :   C_LO = ~B; // logical not 
+				4   :   C_LO = -B; //negation function
+				3   :   C_LO = A - B;
+				2   :   C_LO = A + B;
+				1   :   C_LO = A | B;
+				0   :   C_LO = A & B;
+				default	:	begin end
+			endcase
+		end
 	end
 endmodule
