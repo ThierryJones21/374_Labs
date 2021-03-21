c5c2194f1b53e0c3785ce6e2def239d28e2714f6
Naod Dereje <17nd26@queensu.ca>
2021-03-21 10:01:16 -0400

    testbenches for ld for 4 cases, out testbench minor fixes

--- Lab2/InstructionTestBenches/ldi_case_4_TB.v
+++ Lab2/InstructionTestBenches/ldi_case_4_TB.v
@@ -1,8 +1,9 @@
-// Output Port 
+module ldi_case_3_TB ();
+  
 always @(Present_state)// do the required job ineach state
-begin
-    case (Present_state) //assert the required signals in each clock cycle
-        Default: begin //initialize the signals
+    begin
+        case (Present_state) //assert the required signals in each clock cycle
+            Default: begin //initialize the signals
                 PCout <= 0; Zlowout <= 0; Zhighout <= 0; MDRout<= 0;  //initialize the signals
                 MARin <= 0; Zlowin <= 0; Zhighin <= 0;
                 PCin <= 0; MDRin <= 0; IRin  <= 0; Yin <= 0;
@@ -12,22 +13,35 @@ begin
                 Mdatain <= 32'h00000000; highout <= 0; lowout <= 0;
                 BAout <= 0; Rin <= 0; Rout <= 0; Gra <= 0; Grb <= 0; Grc <= 0; ram_enable <= 0;
                 R15_enable <= 0; PC_enable <= 0;
-        end    
+            end
+            
+    case (Present_state) //assert the required signals in each clock cycle
+            
         T0: begin 
             #10 PCout <= 1; MARin <= 1; IncPC <= 1; Zlowin<= 1;  //initialize the signals         
             #15 PCout <= 0; MARin <= 0; IncPC <= 0; Zlowin<= 0;
         end
         T1: begin
-            #10 Zlowout<= 1; PCin <= 1; Read <= 1; MDRin <= 1; Mdatain <= 32'h00000161; // opcode for out r1 101100001// put Mdatain onto outputport
+            #10 Zlowout<= 1; PCin <= 1; Read <= 1; MDRin <= 1; Mdatain <= 32'h08800023; //Reading from RAM 0000101000000xxxxxxxxxxxxxxxxxx
             #15 Zlowout<= 0; PCin <= 0; Read <= 0; MDRin <= 0;
         end
         T2: begin
-            #10 MDRout<= 1; IRin <= 1; // read MDR into IR
+            #10 MDRout<= 1; IRin <= 1;
             #15 MDRout<= 0; IRin <= 0;
         end
         T3: begin
-            #10 Gra <= 1; Rout <= 1; MDRin <= 1; outPortIn <= 1;// store value of R1 in MDR
-            #15 Gra <= 0; Rout <= 0; MDRin <= 0; outPortIn <= 0;
-		  end
+            #10 Grb <= 1; BAout<= 1; Yin <= 1;
+            #15 Grb <= 0; BAout<= 0; Yin <= 0;
+        end
+        T4: begin
+            #10 Cout<= 1; Mdatain <= 32'h08800023; Zlowin <= 1;
+            #15 Cout<= 0;  Zlowin <= 0;
+        end
+        T5: begin
+            #10 Zlowout<= 1; Gra <= 1; Rin <= 1;
+            #15 Zlowout<= 0; Gra <= 0; Rin <= 0;
+        end
     endcase
 end
+
+endmodule 
