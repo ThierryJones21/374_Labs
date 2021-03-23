1d8a62eb226186aaa9d7e66eafdbc16e61905f26
Jamie Won <jamie.won.2000@gmail.com>
2021-03-22 22:33:47 -0400

    Fixed code for input and output ports

--- Lab2/inputPort.v
+++ Lab2/inputPort.v
@@ -1,8 +1,8 @@
-module inputPort( input [31:0] inputUnit, input clr, clk, output reg [31:0] busMuxIn_In_PortIn);
-//D = InputUnit, Q = BusMuxIn_In.Port, Strobe Unit if desired, clr, clk
-
-	always @(posedge clk) begin
-		busMuxIn_In_PortIn = (clr == 1'b1) ? 0 : inputUnit;
-	end
-
-endmodule
+module inputPort #(parameter VAL = 0)(input clk, clr, strobe, input [31:0] inputUnit, output reg [31:0] busMuxIn_In_PortIn);
+    always@(posedge clk or negedge clr)
+    begin
+        if(clr == 0) busMuxIn_In_PortIn <= 0;
+        else if(strobe) busMuxIn_In_PortIn <= inputUnit;
+    end
+    initial busMuxIn_In_PortIn = VAL; // assigns initial value
+endmodule 
--- Lab2/outputPort.v
+++ Lab2/outputPort.v
@@ -1,10 +1,8 @@
-module outputPort( input [31:0] BusMuxOut, input clr, clk, outPortIn, output reg [31:0] outputUnit);
-//D = BusMuxOut, Q = outputUnit, enable = outPortIn, clr, clk
-  reg tempBus;//one bit
-
-  always @(posedge clk) begin
-    outputUnit = (clr == 1) ? 0 : BusMuxOut;
-
-  end
-
-endmodule
+module outputPort #(parameter VAL = 0) (input clk, clr, outPortIn, input [31:0] BusMuxOut, output reg [31:0] outputUnit);
+    always@(posedge clk or negedge clr)
+    begin
+        if(clr == 0) outputUnit <= 0;
+        else if(outPortIn) outputUnit <= BusMuxOut;
+    end
+    initial outputUnit = VAL; // assigns initial value
+endmodule 
