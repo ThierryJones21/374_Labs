1ff8a3f6f6c4acabefb2f56ebb4a3a1149417ba5
Jamie Won <jamie.won.2000@gmail.com>
2021-03-22 22:32:59 -0400

    Removed unneeded comments

--- Lab2/Register.v
+++ Lab2/Register.v
@@ -6,5 +6,3 @@ module Register #(parameter VAL = 0)(input clk, clr, input [31:0] BusMuxOut, inp
     end
     initial BusMuxIn = VAL; // assigns initial value
 endmodule 
-// It overwrite the 0 when a register is instantiated?
-// TODO - remove regloads !
