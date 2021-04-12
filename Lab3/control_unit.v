2c7b3d4e3e3b31297e5f823bcbd0cf9ae6b712cc
Thierry Jones <17tfj2@queensu.ca>
2021-04-12 16:04:12 -0400

    Tex and Hex

--- Lab3/control_unit.v
+++ Lab3/control_unit.v
@@ -130,17 +130,30 @@ always@(posedge Clock, posedge Reset)// finite state machine; if clock or reset 
 	
 always@(Present_state)// do the job for each state
 	begin case(Present_state)              // assert the required signals in each state
-			Reset_state			:				begin Gra <= 0;   
-														   Grb <= 0;  	 
-															Grc <= 0; 
-															Yin <= 0; //initialize the signals
+			Reset_state			:				begin 	Run <= 1; R_enableIn <= 0;
+														PCout <= 0; Zlowout <= 0; Zhighout <= 0; MDRout<= 0;  //initialize the signals
+            											MARin <= 0; Zlowin <= 0; Zhighin <= 0;
+														PCin <= 0; MDRin <= 0; IRin  <= 0; Yin <= 0;
+														IncPC <= 0; Read <= 0; CONTROL <= 0;
+														Clear <= 1; Yout <= 0; highin <= 0; lowin <= 0;
+														Cout <= 0; outPortIn <= 0; inPortOut <= 0; inPortIn<= 0; con_in <= 0;
+														highout <= 0; lowout <= 0;
+														BAout <= 0; Rin <= 0; Rout <= 0; Gra <= 0; Grb <= 0; Grc <= 0; ram_enable <= 0;
+														R15_enable <= 0; PC_enable <= 0;//initialize the signals
 													end
 													
-			fetch0				:				begin PCout<= 1; // see if you need to de-assert these signals 
-															MARin <= 1;  
-															IncPC <= 1; 
-															Zin <= 0;
-													end 
+			fetch0				:				begin PCout <=1; MARin <=1;
+																								end 
+			fetch1				:				begin Zlowout<= 1; 
+													  PCout <= 0; 
+													  Read <= 1; 
+													  MDRin <= 1; 
+            											MARin <= 0;
+        										end
+	        fetch2				:				begin MDRout<= 1; 
+														IRin <= 1;
+														MDRout<= 0; IRin <= 0;
+												end
 													
 			add3					: 				begin Grb <= 1; 
 															Grb <= 0;
